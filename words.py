f24bd620a7470a1e3c95cd96b8c621515c67bac3
seantodd86 <seantodd86@gmail.com>
2022-08-11 17:46:23 -0400

    updated command lines

--- words.py
+++ words.py
@@ -1,13 +1,54 @@
+"""Retrieve and print words from a URL.
+
+UsageL
+
+    Python3 words.py <URL>
+"""
+
+import sys
 from urllib.request import urlopen
 
 
-story = urlopen('http://sixty-north.com/c/t.txt')
-story_words = []
-for line in story:
-        line_words = line.decode('utf-8').split()
-        for word in line_words:
+def fetch_words(url):
+    """Fetch a list of words from a URL.
+    
+      Args:
+            url: The URL of a UTF-8 text document.
+            
+        Returns:
+            A list of strings containing the words from
+            the document.
+    """
+    story = urlopen(url)
+    story_words = []
+    for line in story:
+        Line_words = line.decode('utf8').split()
+        for word in Line_words:
             story_words.append(word)
-story.close()
+    story.close()
+    return story_words
 
-for word in story_words:
-        print(word)
+def print_items(items):
+    """prtin items one per line.
+    
+        Args:
+            An iterable series of printable items.
+    """
+    for item in items:
+        print(item)
+
+def main(url):
+    """Print each word from a text document from at a URL.
+    
+        Args:
+            url: The URL of a UTF-8 text document.
+    """
+    words = fetch_words(url)
+    print_items(words)
+
+
+if __name__== '__main__':
+        main(url = sys.argv[1])
+
+        
+    
